ee2e45f40cc080f5d314b23cc71502ca3d957888
Nitish <mail.logicninjax@gmail.com>
2026-01-05 10:40:01 +0530

    implement servie method

--- src/main/java/com/nitish/notification_service/service/ClientService.java
+++ src/main/java/com/nitish/notification_service/service/ClientService.java
@@ -1,8 +1,16 @@
 package com.nitish.notification_service.service;
 
 import com.nitish.notification_service.dto.request.ClientRegisterRequest;
+import com.nitish.notification_service.dto.request.ClientUpdateRequest;
 import com.nitish.notification_service.dto.response.ClientRegisterResponse;
+import com.nitish.notification_service.dto.response.ClientUpdateResponse;
+
+import java.util.UUID;
 
 public interface ClientService {
     ClientRegisterResponse registerClient(ClientRegisterRequest request);
+
+    void updateClientStatus(UUID clientId, String status);
+
+    ClientUpdateResponse updateClientInfo(UUID clientId, ClientUpdateRequest request);
 }
--- src/main/java/com/nitish/notification_service/service/impl/ClientServiceImpl.java
+++ src/main/java/com/nitish/notification_service/service/impl/ClientServiceImpl.java
@@ -1,15 +1,22 @@
 package com.nitish.notification_service.service.impl;
 
 import com.nitish.notification_service.dto.request.ClientRegisterRequest;
+import com.nitish.notification_service.dto.request.ClientUpdateRequest;
 import com.nitish.notification_service.dto.response.ClientRegisterResponse;
+import com.nitish.notification_service.dto.response.ClientUpdateResponse;
 import com.nitish.notification_service.entity.Client;
 import com.nitish.notification_service.enums.ClientStatus;
+import com.nitish.notification_service.exception.custom_exception.EntityNotFoundException;
+import com.nitish.notification_service.exception.custom_exception.InvalidStatusException;
 import com.nitish.notification_service.repository.ClientRepository;
 import com.nitish.notification_service.service.ClientService;
 import com.nitish.notification_service.util.mapper.ClientMapper;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ClientServiceImpl implements ClientService {
@@ -31,4 +38,40 @@ public class ClientServiceImpl implements ClientService {
         logger.info("client registered successfully [id={}, name={}]", client.getClientId(), client.getFullName());
         return clientMapper.toRegisterResponse(client);
     }
+
+    @Transactional
+    @Override
+    public void updateClientStatus(UUID clientId, String status) {
+        ClientStatus clientStatus = validateStatus(status);
+
+        int updatedRows = clientRepository.updateClientStatus(clientId, clientStatus);
+        if (updatedRows > 0) logger.info("client status updated successfully [id={}]", clientId);
+    }
+
+    private ClientStatus validateStatus(String status) {
+        ClientStatus clientStatus;
+        try {
+            clientStatus = ClientStatus.valueOf(status.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            logger.error("invalid client status found [value={}]", status);
+            throw new InvalidStatusException("invalid client status [value=%s]".formatted(status));
+        }
+
+        return clientStatus;
+    }
+
+    @Transactional
+    @Override
+    public ClientUpdateResponse updateClientInfo(UUID clientId, ClientUpdateRequest request) {
+        Client client = clientRepository.findById(clientId)
+                .orElseThrow(() -> new EntityNotFoundException("client", clientId));
+
+        client.setFullName(request.getFullName());
+        client.setEmail(request.getEmail());
+        client = clientRepository.save(client);
+        logger.info("client details updated successfully [id={}, name={}]", clientId, client.getFullName());
+        return clientMapper.toUpdateResponse(client);
+    }
+
+
 }
