2820e6cfbbf0509a16acc152c060644d64eb2837
Nitish <mail.logicninjax@gmail.com>
2026-01-09 18:54:51 +0530

    add user dto mapper

---
+++ src/main/java/com/nitish/notification_service/util/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.nitish.notification_service.util.mapper;
+
+import com.nitish.notification_service.dto.request.UserRegisterRequest;
+import com.nitish.notification_service.dto.response.UserDetailsResponse;
+import com.nitish.notification_service.dto.response.UserRegisterResponse;
+import com.nitish.notification_service.dto.response.UserUpdateResponse;
+import com.nitish.notification_service.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "templates", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "notificationRequests", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "client", ignore = true)
+    User toUser(UserRegisterRequest request);
+
+    UserRegisterResponse toRegisterResponse(User user);
+
+    UserDetailsResponse toDetailsResponse(User user);
+
+    UserUpdateResponse toUpdateResponse(User user);
+}
