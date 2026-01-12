cb74618de96a0308283e01ecb69879276833cbc4
Nitish <mail.logicninjax@gmail.com>
2026-01-12 19:21:39 +0530

    ignore irrelevant fields

---
+++ src/main/java/com/nitish/notification_service/util/mapper/TemplateMapper.java
@@ -0,0 +1,23 @@
+package com.nitish.notification_service.util.mapper;
+
+import com.nitish.notification_service.dto.request.CreateTemplateRequest;
+import com.nitish.notification_service.dto.response.CreateTemplateResponse;
+import com.nitish.notification_service.entity.NotificationTemplate;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TemplateMapper {
+
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "templateId", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "placeholders", ignore = true)
+    @Mapping(target = "notificationRequests", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "client", ignore = true)
+    NotificationTemplate toTemplate(CreateTemplateRequest request);
+
+    CreateTemplateResponse toCreateResponse(NotificationTemplate template);
+}
