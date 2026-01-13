b85ac6a539cee55400bb20938223bf23d5890fe4
Nitish <mail.logicninjax@gmail.com>
2026-01-13 13:54:32 +0530

    add service method

--- src/main/java/com/nitish/notification_service/service/TemplateService.java
+++ src/main/java/com/nitish/notification_service/service/TemplateService.java
@@ -1,11 +1,14 @@
 package com.nitish.notification_service.service;
 
 import com.nitish.notification_service.dto.request.CreateTemplateRequest;
+import com.nitish.notification_service.dto.request.UpdateTemplateRequest;
 import com.nitish.notification_service.dto.response.CreateTemplateResponse;
+import com.nitish.notification_service.dto.response.UpdateTemplateResponse;
 
 import java.util.UUID;
 
 public interface TemplateService {
 
     CreateTemplateResponse createTemplate(UUID userId, CreateTemplateRequest request);
+    UpdateTemplateResponse updateTemplate(UUID templateId, UpdateTemplateRequest request);
 }
--- src/main/java/com/nitish/notification_service/service/impl/TemplateServiceImpl.java
+++ src/main/java/com/nitish/notification_service/service/impl/TemplateServiceImpl.java
@@ -1,7 +1,9 @@
 package com.nitish.notification_service.service.impl;
 
 import com.nitish.notification_service.dto.request.CreateTemplateRequest;
+import com.nitish.notification_service.dto.request.UpdateTemplateRequest;
 import com.nitish.notification_service.dto.response.CreateTemplateResponse;
+import com.nitish.notification_service.dto.response.UpdateTemplateResponse;
 import com.nitish.notification_service.entity.Client;
 import com.nitish.notification_service.entity.NotificationTemplate;
 import com.nitish.notification_service.entity.User;
@@ -46,7 +48,7 @@ public class TemplateServiceImpl implements TemplateService {
 
     @Transactional
     @Override
-    public CreateTemplateResponse createTemplate(UUID userId, CreateTemplateRequest request){
+    public CreateTemplateResponse createTemplate(UUID userId, CreateTemplateRequest request) {
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new EntityNotFoundException("user", userId));
@@ -59,12 +61,12 @@ public class TemplateServiceImpl implements TemplateService {
         templateUtil.validateTemplateSyntax(content);
         templateUtil.validateContentLength(content.length(), request.channel());
 
-        if (request.channel() != NotificationChannel.EMAIL && request.contentType() == ContentType.HTML){
+        if (request.channel() != NotificationChannel.EMAIL && request.contentType() == ContentType.HTML) {
             throw new TemplateValidationException("HTML content is only allowed for EMAIL");
         }
 
-        if (request.contentType() == ContentType.HTML){
-            if (templateUtil.isHtml(content)){
+        if (request.contentType() == ContentType.HTML) {
+            if (templateUtil.isHtml(content)) {
                 throw new TemplateValidationException("Declared HTML but content is not HTML");
             }
 
@@ -84,8 +86,8 @@ public class TemplateServiceImpl implements TemplateService {
 
         try {
             template = templateRepository.saveAndFlush(template);
-        }catch (DataIntegrityViolationException e){
-            if (e.getMessage().contains("uk_client_template_name")){
+        } catch (DataIntegrityViolationException e) {
+            if (e.getMessage().contains("uk_client_template_name")) {
                 throw new DuplicateFieldException("template already exists");
             }
             throw e;
@@ -95,5 +97,37 @@ public class TemplateServiceImpl implements TemplateService {
                 template.getTemplateId(), template.getChannel(), template.getCreatedBy().getUserId(), template.getClient().getClientId()
         );
         return templateMapper.toCreateResponse(template);
+    }
+
+    @Transactional
+    @Override
+    public UpdateTemplateResponse updateTemplate(UUID templateId, UpdateTemplateRequest request) {
+        NotificationTemplate template = templateRepository.findById(templateId)
+                .orElseThrow(() -> new EntityNotFoundException("template", templateId));
+
+        if (request.name() != null) template.setName(request.name());
+        if (request.channel() != null) template.setChannel(request.channel());
+        if (request.subject() != null) template.setSubject(request.subject());
+
+        if (request.content() != null) {
+            template.setContent(request.content());
+
+            Set<String> placeHolders = templateUtil.extractPlaceholders(request.content());
+
+            template.setPlaceholders(templateUtil.toJson(placeHolders));
+        }
+
+        try{
+            template = templateRepository.saveAndFlush(template);
+        }catch (DataIntegrityViolationException e){
+            if (e.getMessage().contains("uk_client_template_name")){
+                throw new DuplicateFieldException("template name already exists try a different name");
+            }
+            throw e;
+        }
+
+        logger.info("template record updated successfully [template id={}]", template.getTemplateId());
+
+        return templateMapper.toUpdateResponse(template);
     }
 }
