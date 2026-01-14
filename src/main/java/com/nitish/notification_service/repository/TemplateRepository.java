7fc8bc5b32611fb45bd22064aec4d3ed8b789622
Nitish <mail.logicninjax@gmail.com>
2026-01-14 13:34:30 +0530

    add custom query method

--- src/main/java/com/nitish/notification_service/repository/TemplateRepository.java
+++ src/main/java/com/nitish/notification_service/repository/TemplateRepository.java
@@ -1,9 +1,18 @@
 package com.nitish.notification_service.repository;
 
 import com.nitish.notification_service.entity.NotificationTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface TemplateRepository extends JpaRepository<NotificationTemplate, UUID> {
+
+    @Query("""
+            SELECT nt FROM NotificationTemplate nt
+            WHERE nt.createdBy.userId = :creatorId
+            """)
+    Page<NotificationTemplate> getTemplatesByCreatorId(UUID creatorId, Pageable pageable);
 }
