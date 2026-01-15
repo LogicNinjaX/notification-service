0c8c46e16e00030d82188cac17733944e4117e67
Nitish <mail.logicninjax@gmail.com>
2026-01-15 17:09:25 +0530

    add delete query for template deletion

--- src/main/java/com/nitish/notification_service/repository/TemplateRepository.java
+++ src/main/java/com/nitish/notification_service/repository/TemplateRepository.java
@@ -1,9 +1,11 @@
 package com.nitish.notification_service.repository;
 
 import com.nitish.notification_service.entity.NotificationTemplate;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
@@ -15,4 +17,13 @@ public interface TemplateRepository extends JpaRepository<NotificationTemplate, 
             WHERE nt.createdBy.userId = :creatorId
             """)
     Page<NotificationTemplate> getTemplatesByCreatorId(UUID creatorId, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("""
+            DELETE FROM NotificationTemplate nt
+            WHERE nt.templateId = :templateId
+            AND nt.createdBy.userId = :userId
+            """)
+    int deleteTemplateById(UUID templateId, UUID userId);
 }
