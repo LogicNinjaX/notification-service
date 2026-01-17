43bfca7ea6db28b0c76b2b30c53681f0701e77af
Nitish <mail.logicninjax@gmail.com>
2026-01-17 15:02:31 +0530

    add event entity to handle kafka events

---
+++ src/main/java/com/nitish/notification_service/entity/OutBoxEvent.java
@@ -0,0 +1,91 @@
+package com.nitish.notification_service.entity;
+
+import com.nitish.notification_service.enums.AggregateType;
+import com.nitish.notification_service.enums.EventStatus;
+import com.nitish.notification_service.enums.EventType;
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class OutBoxEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID eventId;
+
+    @Column(nullable = false)
+    private AggregateType aggregateType;
+
+    @Column(nullable = false)
+    private UUID aggregateId;
+
+    @Column(nullable = false)
+    private EventType eventType;
+
+    private String payload;
+
+    private EventStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public UUID getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(UUID eventId) {
+        this.eventId = eventId;
+    }
+
+    public AggregateType getAggregateType() {
+        return aggregateType;
+    }
+
+    public void setAggregateType(AggregateType aggregateType) {
+        this.aggregateType = aggregateType;
+    }
+
+    public UUID getAggregateId() {
+        return aggregateId;
+    }
+
+    public void setAggregateId(UUID aggregateId) {
+        this.aggregateId = aggregateId;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public EventStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EventStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
