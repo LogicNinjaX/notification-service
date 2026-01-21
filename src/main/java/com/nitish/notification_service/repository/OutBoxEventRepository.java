80e7040c14ea1a27ff4d00c9fd91635b5514c1ed
Nitish <mail.logicninjax@gmail.com>
2026-01-21 16:56:16 +0530

    add custom query method

--- src/main/java/com/nitish/notification_service/repository/OutBoxEventRepository.java
+++ src/main/java/com/nitish/notification_service/repository/OutBoxEventRepository.java
@@ -1,9 +1,28 @@
 package com.nitish.notification_service.repository;
 
 import com.nitish.notification_service.entity.OutBoxEvent;
+import com.nitish.notification_service.enums.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OutBoxEventRepository extends JpaRepository<OutBoxEvent, UUID> {
+
+    @Query("""
+            SELECT obe FROM OutBoxEvent obe
+            WHERE obe.status = :eventStatus
+            LIMIT = 50
+            """)
+    List<OutBoxEvent> getEventsByStatus(EventStatus eventStatus);
+
+    @Modifying
+    @Query("""
+            UPDATE OutBoxEvent obe
+            SET obe.status = :eventStatus
+            WHERE obe.eventId = :eventId
+            """)
+    void updateEventStatus(UUID eventId, EventStatus eventStatus);
 }
