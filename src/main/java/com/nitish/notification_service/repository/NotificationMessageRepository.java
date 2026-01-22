33b6f25ebab118e0a6fc86f68ca6fcd4ae789232
Nitish <mail.logicninjax@gmail.com>
2026-01-22 16:22:23 +0530

    add custom query method

--- src/main/java/com/nitish/notification_service/repository/NotificationMessageRepository.java
+++ src/main/java/com/nitish/notification_service/repository/NotificationMessageRepository.java
@@ -2,8 +2,19 @@ package com.nitish.notification_service.repository;
 
 import com.nitish.notification_service.entity.NotificationMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface NotificationMessageRepository extends JpaRepository<NotificationMessage, UUID> {
+
+    @Query("""
+            SELECT nm FROM NotificationMessage nm
+            JOIN FETCH nm.request
+            LEFT JOIN FETCH nm.notificationDeliveries
+            WHERE nm.messageId = :messageId
+            """)
+    Optional<NotificationMessage> findMessageWithRequestEntity(UUID messageId);
 }
