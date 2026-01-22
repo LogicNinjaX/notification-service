d8dc908214654d006fbe8b625b0e82d1f947003f
Nitish <mail.logicninjax@gmail.com>
2026-01-22 16:21:12 +0530

    add custom exception

---
+++ src/main/java/com/nitish/notification_service/exception/custom_exception/NotificationException.java
@@ -0,0 +1,7 @@
+package com.nitish.notification_service.exception.custom_exception;
+
+public class NotificationException extends RuntimeException {
+    public NotificationException(String message) {
+        super(message);
+    }
+}
