b2958238e66937dd8dc7e12dd75d7a3dafefa18e
Nitish <mail.logicninjax@gmail.com>
2026-01-25 09:09:05 +0530

    renamed file for redability

--- src/main/java/com/nitish/notification_service/producer/OutBoxPublisher.java
+++ src/main/java/com/nitish/notification_service/producer/OutBoxPublisher.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class NotificationMessageProducer {
+public class OutBoxPublisher {
 
     private final OutBoxEventRepository eventRepository;
     private final KafkaTemplate<String, OutBoxEvent> kafkaTemplate;
