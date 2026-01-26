ff7094632bb7afd6764a7afed5fbb7443414d950
Nitish <mail.logicninjax@gmail.com>
2026-01-26 13:49:20 +0530

    add jpa query method

--- src/main/java/com/nitish/notification_service/repository/NotificationMessageRepository.java
+++ src/main/java/com/nitish/notification_service/repository/NotificationMessageRepository.java
@@ -1,11 +1,13 @@
 package com.nitish.notification_service.repository;
 
 import com.nitish.notification_service.entity.NotificationMessage;
+import com.nitish.notification_service.entity.NotificationRequest;
 import com.nitish.notification_service.enums.MessageStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -26,4 +28,7 @@ public interface NotificationMessageRepository extends JpaRepository<Notificatio
             AND nm.status = :status
             """)
     List<NotificationMessage> findAllRetryableMessages(int maxRetries, MessageStatus status, Pageable pageable);
+
+    boolean existsByRequestAndStatusIn(NotificationRequest request, Collection<MessageStatus> statuses);
+
 }
