0b673c712364461644b69de3bba5e5a16193faa4
Nitish <mail.logicninjax@gmail.com>
2026-01-27 19:06:16 +0530

    add jpa query method

--- src/main/java/com/nitish/notification_service/repository/UserRepository.java
+++ src/main/java/com/nitish/notification_service/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
@@ -24,4 +25,12 @@ public interface UserRepository extends JpaRepository<User, UUID> {
             WHERE u.userId = :userId
             """)
     int updateUserRole(UUID userId, UserRole userRole);
+
+    @Query("""
+            SELECT u FROM User u
+            JOIN FETCH u.client client
+            WHERE u.userId = :userId
+            AND client.clientId = :clientId
+            """)
+    Optional<User> findUserWithClient(UUID userId, UUID clientId);
 }
