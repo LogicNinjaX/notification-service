4e121a8d228ff1e80828f1c6e489541008f6821d
Nitish <mail.logicninjax@gmail.com>
2026-01-30 19:57:56 +0530

    implement sms sender using twilio

--- src/main/java/com/nitish/notification_service/channel/SmsNotificationSender.java
+++ src/main/java/com/nitish/notification_service/channel/SmsNotificationSender.java
@@ -1,12 +1,23 @@
 package com.nitish.notification_service.channel;
 
 import com.nitish.notification_service.entity.NotificationMessage;
-import com.nitish.notification_service.entity.OutBoxEvent;
 import com.nitish.notification_service.enums.NotificationChannel;
+import com.nitish.notification_service.exception.custom_exception.NotificationException;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SmsNotificationSender implements NotificationSender{
+public class SmsNotificationSender implements NotificationSender {
+
+    private static final Logger logger = LoggerFactory.getLogger(SmsNotificationSender.class);
+
+    @Value("${app.notification.channel-type.sms.twilio.phone-number}")
+    private String fromNumber;
+
     @Override
     public NotificationChannel channel() {
         return NotificationChannel.SMS;
@@ -14,6 +25,17 @@ public class SmsNotificationSender implements NotificationSender{
 
     @Override
     public void send(NotificationMessage message) {
+        try {
+            Message sms = Message.creator(
+                            new PhoneNumber(message.getRecipient()),
+                            new PhoneNumber(fromNumber),
+                            message.getRenderedContent())
+                    .create();
 
+            logger.info("sms sent successfully to: {}, sent at: {}", message.getRecipient(), sms.getDateSent());
+        } catch (Exception e) {
+            logger.error("failed to send sms to: {}", message.getRecipient(), e);
+            throw new NotificationException(e.getMessage());
+        }
     }
 }
