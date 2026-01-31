bbc73fba2dface8c446f77cf89290c6dd42a622f
Nitish <mail.logicninjax@gmail.com>
2026-01-31 15:27:37 +0530

    add email configration class

---
+++ src/main/java/com/nitish/notification_service/config/EmailNotificationProperties.java
@@ -0,0 +1,17 @@
+package com.nitish.notification_service.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Set;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.notification.channel-type.email")
+public class EmailNotificationProperties {
+
+    private Set<String> allowedDomains;
+
+    public Set<String> getAllowedDomains() {
+        return allowedDomains;
+    }
+}
