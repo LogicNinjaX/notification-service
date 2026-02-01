2197ddf212b3642a2f371161e78d93e92ea8e33f
Nitish <mail.logicninjax@gmail.com>
2026-02-01 19:29:20 +0530

    add validator

--- src/main/java/com/nitish/notification_service/dto/request/SendNotificationRequest.java
+++ src/main/java/com/nitish/notification_service/dto/request/SendNotificationRequest.java
@@ -1,10 +1,13 @@
 package com.nitish.notification_service.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.Map;
 import java.util.UUID;
 
 public record SendNotificationRequest
         (
+                @NotBlank(message = "{template.id.not.blank}")
                 UUID templateId,
                 Map<String, Object> variables,
                 String[] recipients
