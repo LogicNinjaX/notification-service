f43b294e107b5f8ede3aaf19d002a06dc90e689c
Nitish <mail.logicninjax@gmail.com>
2026-02-05 21:32:25 +0530

    add jpa query

--- src/main/java/com/nitish/notification_service/repository/ClientRepository.java
+++ src/main/java/com/nitish/notification_service/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.nitish.notification_service.repository;
 
 import com.nitish.notification_service.entity.Client;
 import com.nitish.notification_service.enums.ClientStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +19,10 @@ public interface ClientRepository extends JpaRepository<Client, UUID> {
             WHERE cl.clientId = :clientId
             """)
     int updateClientStatus(UUID clientId, ClientStatus status);
+
+    @Query("""
+            select cl from Client cl
+            where cl.status = :status
+            """)
+    Page<Client> findAllClientsByStatus(ClientStatus status, Pageable pageable);
 }
