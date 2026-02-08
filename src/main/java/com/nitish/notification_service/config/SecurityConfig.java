db7a3678675569cc43caec37854a40bfdae341e7
Nitish <mail.logicninjax@gmail.com>
2026-02-08 20:21:42 +0530

    enable method security

--- src/main/java/com/nitish/notification_service/config/SecurityConfig.java
+++ src/main/java/com/nitish/notification_service/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final AuthenticationEntryPoint authenticationEntryPoint;
@@ -38,6 +40,7 @@ public class SecurityConfig {
                 requestMatcher ->
                         requestMatcher.requestMatchers("/api/auth/login").permitAll()
                                 .requestMatchers("/api/auth/register").permitAll()
+                                .requestMatchers("/api/v1/client/**").permitAll()
                                 .anyRequest().authenticated()
         );
 
