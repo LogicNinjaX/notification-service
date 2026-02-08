f5adab3eb225c7c48f31a9450048c2767d8323b4
Nitish <mail.logicninjax@gmail.com>
2026-02-08 20:22:48 +0530

    implement rbac

--- src/main/java/com/nitish/notification_service/controller/UserController.java
+++ src/main/java/com/nitish/notification_service/controller/UserController.java
@@ -7,11 +7,14 @@ import com.nitish.notification_service.dto.response.UserDetailsResponse;
 import com.nitish.notification_service.dto.response.UserRegisterResponse;
 import com.nitish.notification_service.dto.response.UserUpdateResponse;
 import com.nitish.notification_service.enums.UserRole;
+import com.nitish.notification_service.security.CustomUserDetails;
 import com.nitish.notification_service.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
@@ -27,6 +30,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(path = "/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiResponse<UserRegisterResponse>> registerUser(@RequestBody @Valid UserRegisterRequest request) {
         var response = userService.registerUser(request);
@@ -34,6 +38,7 @@ public class UserController {
                 .body(ApiResponse.success(response, "user record saved successfully"));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(path = "/user/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiResponse<UserDetailsResponse>> getUserDetails(@PathVariable UUID userId) {
         var response = userService.getUser(userId);
@@ -41,6 +46,14 @@ public class UserController {
                 .body(ApiResponse.success(response, "user record fetched successfully"));
     }
 
+    @GetMapping(path = "/user/my", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<UserDetailsResponse>> getUserDetails(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        var response = userService.getUser(userDetails.getUserId());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(response, "user record fetched successfully"));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping(path = "/user/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiResponse<UserUpdateResponse>> updateUserDetails(@PathVariable UUID userId, @RequestBody @Valid UserUpdateRequest request) {
         var response = userService.updateUserDetails(userId, request);
@@ -48,12 +61,27 @@ public class UserController {
                 .body(ApiResponse.success(response, "user record updated successfully"));
     }
 
+    @PatchMapping(path = "/user/my", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<UserUpdateResponse>> updateUserDetails(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody @Valid UserUpdateRequest request) {
+        var response = userService.updateUserDetails(userDetails.getUserId(), request);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(response, "user record updated successfully"));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping(path = "/user/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable UUID userId) {
         userService.deleteUser(userId);
         return ResponseEntity.noContent().build();
     }
 
+    @DeleteMapping(path = "/user/my")
+    public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        userService.deleteUser(userDetails.getUserId());
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping(path = "/user/{userId}/role")
     public ResponseEntity<Void> updateUserRole(@PathVariable UUID userId, @RequestParam UserRole role) {
         userService.updateUserRole(userId, role);
