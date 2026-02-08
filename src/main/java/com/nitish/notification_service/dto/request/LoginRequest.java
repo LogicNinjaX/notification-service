f5f46dfd50e1282d5160563f21cf208a88e86301
Nitish <mail.logicninjax@gmail.com>
2026-02-08 10:26:22 +0530

    add login dto

---
+++ src/main/java/com/nitish/notification_service/dto/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.nitish.notification_service.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record LoginRequest(
+
+        @NotBlank(message = "{user.username.not-blank}")
+        String username,
+
+        @NotBlank(message = "{user.password.not-blank}")
+        @Length(min = 8, max = 15, message = "Password must be between {min} and {max} characters")
+        String password
+) {
+}
