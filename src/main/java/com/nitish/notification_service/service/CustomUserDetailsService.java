a2a04cc63425ceef656c241a545ec32d79416fd3
Nitish <mail.logicninjax@gmail.com>
2026-02-08 10:25:04 +0530

    add custom user details

---
+++ src/main/java/com/nitish/notification_service/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.nitish.notification_service.service;
+
+import com.nitish.notification_service.entity.User;
+import com.nitish.notification_service.repository.UserRepository;
+import com.nitish.notification_service.security.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
