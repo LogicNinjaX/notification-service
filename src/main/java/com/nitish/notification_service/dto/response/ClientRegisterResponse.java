d09f5054b45267a38ae42c7062325b36226d2157
Nitish <mail.logicninjax@gmail.com>
2026-02-09 10:33:19 +0530

    update dto

--- src/main/java/com/nitish/notification_service/dto/response/ClientRegisterResponse.java
+++ src/main/java/com/nitish/notification_service/dto/response/ClientRegisterResponse.java
@@ -1,20 +1,10 @@
 package com.nitish.notification_service.dto.response;
 
-import com.nitish.notification_service.enums.ClientStatus;
-
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record ClientRegisterResponse
         (
                 UUID clientId,
-
-                String fullName,
-
-                String email,
-
-                ClientStatus status,
-
-                LocalDateTime createdAt
+                UUID userId
         ) { }
 
