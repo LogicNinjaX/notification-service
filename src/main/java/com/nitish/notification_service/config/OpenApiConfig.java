f484f43b7e6e811272a8164986e1e3a8b2fcc7d8
Nitish <mail.logicninjax@gmail.com>
2026-02-23 21:58:34 +0530

    add open api configuration class

---
+++ src/main/java/com/nitish/notification_service/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.nitish.notification_service.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme ="bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Notification Service Api")
+                                .version("1.0")
+                                .description("Api Documentation for Notification Service")
+                ).addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
